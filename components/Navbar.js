0a3531e8d17cba25ff5658cff3636eede0b9c55a
Harichandana M <mharichandanam@gmail.com>
2024-06-04 13:05:31 +0530

    Initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="../images/react-icon-small.png" className="logo-img"/>
+                <h3 className="logo-text">ReactFacts</h3>                
+            </div>
+            <h4 className="logo-title">React Course - Project 1</h4>
+        </nav>
+    );
+}
